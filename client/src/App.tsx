3a87cada5370453cc72a9f0496d3bbb678181b91
Wilfred <wilfred05777@gmail.com>
2023-01-21 09:43:42 +0800

    navbar styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.scss";
+import SidebarNav from "./components/sidebarnav/SidebarNav";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex">
-      
+    <div className="flex ">
+      <SidebarNav/>
     </div>
   );
 }
--- client/src/components/sidebarnav/SidebarNav.tsx
+++ client/src/components/sidebarnav/SidebarNav.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+// icons react-icons
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs";
+import {
+  TbCameraPlus,
+  TbLetterT,
+  TbMusic,
+  TbNote,
+  TbPencil,
+  TbQuestionMark,
+  TbSettings,
+  TbUnderline,
+  TbVideo
+} from "react-icons/tb";
 
-import './sidebarnav.scss'
+import "./sidebarnav.scss";
 
 const SidebarNav = () => {
   return (
-    <div>SidebarNav</div>
-  )
-}
+    <div
+      className="fixed top-2 left-0 h-screen w-16 m-0 flex flex-col
+     bg-white-100 text-black-700 shadow-lg align-baseline"
+    >
+      <SideBarIcon icon={<TbSettings size="28" />} iconTitle={"Settings"} />
+      <SideBarIcon icon={<TbCameraPlus size="28" />} iconTitle={"Media"} />
+      <SideBarIcon icon={<TbMusic size="28" />} iconTitle={"Audio"} />
+      <SideBarIcon icon={<TbUnderline size="28" />} iconTitle={"Sub Titles"} />
+      <SideBarIcon icon={<TbNote size="28" />} iconTitle={"Notes"} />
+      <SideBarIcon icon={<TbVideo size="28" />} iconTitle={"Record"} />
+      <SideBarIcon icon={<TbPencil size="28" />} iconTitle={"Record"} />
+      <SideBarIcon icon={<TbQuestionMark size="28" />} iconTitle={""} />
+    </div>
+  );
+};
 
-export default SidebarNav
+const SideBarIcon = ({ icon, iconTitle }: { icon: any; iconTitle: any }) => {
+  return (
+    <>
+      <div className="sidebar-icon">{icon}</div>
+      <span className="sidebar-text">{iconTitle}</span>
+    </>
+  );
+};
+export default SidebarNav;
